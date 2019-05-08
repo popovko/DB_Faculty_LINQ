472e1bcfdd551b5693b9e75abf3ff5ca91c57cfd
Klim <popov_k.o@ukr.net>
2019-05-08 10:28:39 +0300

    Add some buttons in mainForm

--- DB_Faculty_LINQ/DB_Faculty_LINQ/MainForm.Designer.cs
+++ DB_Faculty_LINQ/DB_Faculty_LINQ/MainForm.Designer.cs
@@ -30,38 +30,90 @@
         {
             this.dgv = new System.Windows.Forms.DataGridView();
             this.cbTables = new System.Windows.Forms.ComboBox();
+            this.lblTables = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnAddLesson = new System.Windows.Forms.Button();
+            this.btnGetTimetable = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
             this.SuspendLayout();
             // 
             // dgv
             // 
+            this.dgv.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv.Location = new System.Drawing.Point(89, 70);
+            this.dgv.Location = new System.Drawing.Point(12, 12);
             this.dgv.Name = "dgv";
-            this.dgv.Size = new System.Drawing.Size(367, 260);
+            this.dgv.Size = new System.Drawing.Size(393, 426);
             this.dgv.TabIndex = 0;
             // 
             // cbTables
             // 
             this.cbTables.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.cbTables.FormattingEnabled = true;
-            this.cbTables.Location = new System.Drawing.Point(588, 70);
+            this.cbTables.Location = new System.Drawing.Point(610, 49);
             this.cbTables.Name = "cbTables";
             this.cbTables.Size = new System.Drawing.Size(121, 21);
             this.cbTables.TabIndex = 2;
             this.cbTables.SelectedIndexChanged += new System.EventHandler(this.cbTables_SelectedIndexChanged);
+            // 
+            // lblTables
+            // 
+            this.lblTables.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTables.AutoSize = true;
+            this.lblTables.Location = new System.Drawing.Point(493, 52);
+            this.lblTables.Name = "lblTables";
+            this.lblTables.Size = new System.Drawing.Size(46, 13);
+            this.lblTables.TabIndex = 3;
+            this.lblTables.Text = "Таблиці";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAdd.Location = new System.Drawing.Point(610, 123);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(121, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Додати в таблицю";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnAddLesson
+            // 
+            this.btnAddLesson.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAddLesson.Location = new System.Drawing.Point(610, 209);
+            this.btnAddLesson.Name = "btnAddLesson";
+            this.btnAddLesson.Size = new System.Drawing.Size(121, 23);
+            this.btnAddLesson.TabIndex = 5;
+            this.btnAddLesson.Text = "Додати пару";
+            this.btnAddLesson.UseVisualStyleBackColor = true;
+            // 
+            // btnGetTimetable
+            // 
+            this.btnGetTimetable.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGetTimetable.Location = new System.Drawing.Point(610, 308);
+            this.btnGetTimetable.Name = "btnGetTimetable";
+            this.btnGetTimetable.Size = new System.Drawing.Size(121, 23);
+            this.btnGetTimetable.TabIndex = 6;
+            this.btnGetTimetable.Text = "Показати розклад";
+            this.btnGetTimetable.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGetTimetable);
+            this.Controls.Add(this.btnAddLesson);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblTables);
             this.Controls.Add(this.cbTables);
             this.Controls.Add(this.dgv);
             this.Name = "MainForm";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +121,10 @@
 
         private System.Windows.Forms.DataGridView dgv;
         private System.Windows.Forms.ComboBox cbTables;
+        private System.Windows.Forms.Label lblTables;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnAddLesson;
+        private System.Windows.Forms.Button btnGetTimetable;
     }
 }
 
